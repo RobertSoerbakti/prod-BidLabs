b93046032754c371c8a48f7d7c79a92f8005f59f
Robert Soerbakti <rsuranta@dyned.com>
2020-07-05 16:13:24 +0700

    #new build

---
+++ docs/precache-manifest.29eb86b268611a7b63aec7efe937450e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c79cf6848497198f377",
+    "url": "css/app.5f637b6b.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d3a27a5685343568632aefbcfc3d8e93",
+    "url": "index.html"
+  },
+  {
+    "revision": "63fae321522787b65aed",
+    "url": "js/about.c48af40f.js"
+  },
+  {
+    "revision": "4c79cf6848497198f377",
+    "url": "js/app.d2f650c0.js"
+  },
+  {
+    "revision": "1749a7d14aa29e251baa",
+    "url": "js/chunk-vendors.c97a7ce9.js"
+  },
+  {
+    "revision": "129c5ced6bc4f0ab09f455367e632d09",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
